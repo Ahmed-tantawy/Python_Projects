623a3d946eb6b4a4476a0fb5c7926d991d8782b6
Ahmed-Tantawy <ahmedtantawyy2001@gmail.com>
2025-09-23 13:56:36 +0200

    adding conversion examples

--- boolean.py
+++ boolean.py
@@ -45,3 +45,71 @@ if not age > 18:
     print("You are not eligible to vote")
 else:
     print("You are eligible to vote")
+
+#---------------------------------------
+#----Assignment Operators-----
+#---------------------------------------
+# =, +=, -=, *=, /=, %=, //=, **=
+#---------------------------------------
+
+name = "John"
+surname = "Doe"
+print(name == surname)  
+print(name != surname)
+print(name > surname)
+print(name < surname)
+
+#---------------------------------------
+#----Type Conversion-----
+#---------------------------------------
+# int(), float(), str(), bool()
+#---------------------------------------
+# str
+a = 10
+print(type(a))
+print(type(str(a)))
+
+#  list
+c = "jhon" #string
+d =(1,2,3,4,5) #tuple
+e ={"A", "B", "C"} #set
+f = {"A":1, "B":2} #dict
+
+print(list(c))
+print(list(d))
+print(list(e))
+print(list(f))
+
+# tuple
+c = "jhon" #string
+d =[1,2,3,4,5] #list
+e ={"A", "B", "C"} #set
+f = {"A":1, "B":2} #dict
+
+print(tuple(c))
+print(tuple(d))
+print(tuple(e))
+print(tuple(f))
+
+# set
+c = "jhon" #string
+d =[1,2,3,4,5] #list
+e =("A", "B", "C") #tuple
+f = {"A":1, "B":2} #dict
+
+print(set(c))
+print(set(d))
+print(set(e))
+print(set(f))
+
+# dict
+
+# c = "jhon" #string
+d =[[1, "one"],[2, "two"], [3, "three"]] #list
+e =(("A", 1), ("B", 2), ("C", 3)) #set
+# f = {"A", "B"} #set unhashable type
+
+# print(dict(c))
+print(dict(d))
+print(dict(e))
+# print(dict(f))
